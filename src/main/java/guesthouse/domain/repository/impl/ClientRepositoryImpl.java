10ccdd5f6ba45009defe77b9310c1ede9d22b932
Adam Czechowski <Adam Czechowski@Adam.home>
2017-05-18 23:12:23 +0200

    Correct hibernate connection

--- src/main/java/guesthouse/domain/repository/impl/ClientRepositoryImpl.java
+++ src/main/java/guesthouse/domain/repository/impl/ClientRepositoryImpl.java
@@ -2,17 +2,12 @@ package guesthouse.domain.repository.impl;
 
 import java.util.List;
 
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 
 import guesthouse.domain.Client;
 import guesthouse.domain.repository.ClientRepository;
@@ -23,24 +18,15 @@ public class ClientRepositoryImpl implements ClientRepository {
 
 	private static final Logger logger = LoggerFactory.getLogger(ClientRepositoryImpl.class);
 
+	@Autowired
 	private SessionFactory sessionFactory;
 	
 	@SuppressWarnings("unchecked")
 	public List<Client> getAllClients() {
 
 		Session session = this.sessionFactory.getCurrentSession();
-		List<Client> clientList = session.createQuery("from client").list();
+		List<Client> clientList = session.createQuery("from Client").list();
 		return clientList;
-	}
-	
-
-	
-	public SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
-
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
 	}
 
 }
--- src/main/java/guesthouse/service/impl/ClientServiceImpl.java
+++ src/main/java/guesthouse/service/impl/ClientServiceImpl.java
@@ -2,19 +2,24 @@ package guesthouse.service.impl;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import guesthouse.domain.Client;
 import guesthouse.domain.repository.ClientRepository;
 import guesthouse.service.ClientService;
 
 @Service
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class ClientServiceImpl implements ClientService {
 
 	@Autowired
 	private ClientRepository clientRepository;
 	
+	@Transactional(readOnly = false)
 	public List<Client> getAllClients() {
 		return clientRepository.getAllClients();
 	}
